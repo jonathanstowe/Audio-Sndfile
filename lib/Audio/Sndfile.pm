b296ec3da6a8f8501af3038069301264d0a6d435
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-06-13 21:32:46 +0100

    Added writes but not as working as I thought

--- lib/Audio/Sndfile.pm
+++ lib/Audio/Sndfile.pm
@@ -34,11 +34,26 @@ class Audio::Sndfile {
             @tmp_arr;
         }
 
+        method !write-write(Audio::Sndfile::Info $info, &write-sub, $buff, @items) returns Int {
+            $buff[$_] = @items[$_] for ^@items.elems;
+
+            my Int $frames = (@items.elems / $info.channels).Int;
+            &write-sub(self, $buff, $frames);
+        }
+
         sub sf_readf_short(File , CArray[int16], int64) returns int64 is native('libsndfile') { * }
+
 
         method read-short(Int $frames, Audio::Sndfile::Info $info) returns Array {
             my $buff =  CArray[int16].new;
             self!read-read($frames, $info, &sf_readf_short, $buff);
+        }
+
+        sub sf_writef_short(File , CArray[int16], int64) returns int64 is native('libsndfile') { * }
+
+        method write-short(Audio::Sndfile::Info $info, @items ) returns Int {
+            my $buff = CArray[int16].new;
+            self!write-write($info, &sf_writef_short, $buff, @items);
         }
 
         sub sf_readf_int(File , CArray[int32], int64) returns int64 is native('libsndfile') { * }
@@ -48,6 +63,13 @@ class Audio::Sndfile {
             self!read-read($frames, $info, &sf_readf_int, $buff);
         }
 
+        sub sf_writef_int(File , CArray[int32], int64) returns int64 is native('libsndfile') { * }
+
+        method write-int(Audio::Sndfile::Info $info, @items ) returns Int {
+            my $buff = CArray[int32].new;
+            self!write-write($info, &sf_writef_int, $buff, @items);
+        }
+
         sub sf_readf_double(File , CArray[num64], int64) returns int64 is native('libsndfile') { * }
 
         method read-double(Int $frames, Audio::Sndfile::Info $info) returns Array {
@@ -55,11 +77,26 @@ class Audio::Sndfile {
             self!read-read($frames, $info, &sf_readf_double, $buff);
         }
 
+        sub sf_writef_double(File , CArray[num64], int64) returns int64 is native('libsndfile') { * }
+
+        method write-double(Audio::Sndfile::Info $info, @items ) returns Int {
+            note @items.perl;
+            my $buff = CArray[num64].new;
+            self!write-write($info, &sf_writef_double, $buff, @items);
+        }
+
         sub sf_readf_float(File , CArray[num32], int64) returns int64 is native('libsndfile') { * }
 
         method read-float(Int $frames, Audio::Sndfile::Info $info) returns Array {
             my $buff =  CArray[num32].new;
             self!read-read($frames, $info, &sf_readf_float, $buff);
+        }
+
+        sub sf_writef_float(File , CArray[num32], int64) returns int64 is native('libsndfile') { * }
+
+        method write-float(Audio::Sndfile::Info $info, @items ) returns Int {
+            my $buff = CArray[num32].new;
+            self!write-write($info, &sf_writef_float, $buff, @items);
         }
 
         sub sf_write_sync(File) is native('libsndfile') { * }
@@ -85,13 +122,14 @@ class Audio::Sndfile {
 
     sub sf_open(Str $filename, int32 $mode, Audio::Sndfile::Info $info) returns File is native('libsndfile') { * }
 
-    submethod BUILD(Str() :$!filename!, Bool :$r, Bool :$w, Bool :$rw, *%info) {
+    submethod BUILD(Str() :$!filename!, Bool :$r, Bool :$w, Bool :$rw, Audio::Sndfile::Info :$!info?,  *%info) {
         if one($r, $w, $rw ) {
             $!mode = do if $r {
                 Read;
             }
             elsif $w {
-                $!info = Audio::Sndfile::Info.new(%info);
+                $!info //= Audio::Sndfile::Info.new(|%info);
+                die "invalid format supplied to :w" if not $!info.format-check;
                 Write;
             }
             else {
@@ -116,16 +154,51 @@ class Audio::Sndfile {
         $!file.read-short($frames, $!info);
     }
 
+    method write-short(@frames) returns Int {
+        self!assert-frame-length(@frames);
+        $!file.write-short($!info, @frames);
+    }
+
     method read-int(Int $frames) returns Array {
         $!file.read-int($frames, $!info);
     }
         
+    method write-int(@frames) returns Int {
+        self!assert-frame-length(@frames);
+        $!file.write-int($!info, @frames);
+    }
+
     method read-float(Int $frames) returns Array {
         $!file.read-float($frames, $!info);
     }
         
+    method write-float(@frames) returns Int {
+        self!assert-frame-length(@frames);
+        $!file.write-float($!info, @frames);
+    }
+
     method read-double(Int $frames) returns Array {
         $!file.read-double($frames, $!info);
+    }
+
+    method write-double(@frames) returns Int {
+        self!assert-frame-length(@frames);
+        $!file.write-double($!info, @frames);
+    }
+
+    method !assert-frame-length(@frames) {
+        if (@frames.elems % $!info.channels) != 0 {
+            die "items not a multiple of channels";
+        }
+    }
+
+    # minimum detail required
+    method clone-info() {
+        Audio::Sndfile::Info.new(
+                                    samplerate  => $!info.samplerate,
+                                    channels    => $!info.channels,
+                                    format      => $!info.format
+                                );
     }
 
     method Numeric() {
--- t/020-write.t
+++ t/020-write.t
@@ -241,25 +241,71 @@ for @tests -> $file {
     my @ints = $obj.read-int(100);
     my $int-name = $test-output.child("int-$basename");
 
+    my $i;
+
     my $int-obj;
     lives-ok { $int-obj = Audio::Sndfile.new(filename => $int-name, info => $cinfo, :w) }, "open $int-name for writing";
-
+    lives-ok { $i = $int-obj.write-int(@ints) }, "write-int";
+    is($i, 100, "and it did write 100 frames");
+    lives-ok { $int-obj.close }, "close that file to reopen";
+    lives-ok { $int-obj = Audio::Sndfile.new(filename => $int-name, :r) }, "re-open $int-name for reading";
+    is($int-obj.format, $obj.format, "format is the same as we expected");
+    is($int-obj.channels, $obj.channels, "channels is what we expected");
+    is($int-obj.frames, 100, "and it has 100 frames");
+    my @read-ints = $int-obj.read-int(100);
+    ok(@read-ints ~~ @ints, "and we got back the ints we expected");
+    $int-obj.close;
 
 
     my @shorts = $obj.read-short(100);
     my $short-name = $test-output.child("short-$basename");
+    my $short-obj;
+    lives-ok { $short-obj = Audio::Sndfile.new(filename => $short-name, info => $cinfo, :w) }, "open $short-name for writing";
+    lives-ok { $i = $short-obj.write-short(@shorts) }, "write-short";
+    is($i, 100, "and it did write 100 frames");
+    lives-ok { $short-obj.close }, "close that file to reopen";
+    lives-ok { $short-obj = Audio::Sndfile.new(filename => $short-name, :r) }, "re-open $short-name for reading";
+    is($short-obj.format, $obj.format, "format is the same as we expected");
+    is($short-obj.channels, $obj.channels, "channels is what we expected");
+    is($short-obj.frames, 100, "and it has 100 frames");
+    my @read-shorts = $short-obj.read-short(100);
+    ok(@read-shorts ~~ @shorts, "and we got back the shorts we expected");
+    $short-obj.close;
     my @floats = $obj.read-float(100);
     my $float-name = $test-output.child("float-$basename");
+    my $float-obj;
+    lives-ok { $float-obj = Audio::Sndfile.new(filename => $float-name, info => $cinfo, :w) }, "open $float-name for writing";
+    lives-ok { $i = $float-obj.write-float(@floats) }, "write-float";
+    is($i, 100, "and it did write 100 frames");
+    lives-ok { $float-obj.close }, "close that file to reopen";
+    lives-ok { $float-obj = Audio::Sndfile.new(filename => $float-name, :r) }, "re-open $float-name for reading";
+    is($float-obj.format, $obj.format, "format is the same as we expected");
+    is($float-obj.channels, $obj.channels, "channels is what we expected");
+    is($float-obj.frames, 100, "and it has 100 frames");
+    my @read-floats = $float-obj.read-float(100);
+    ok(@read-floats ~~ @floats, "and we got back the floats we expected");
+    $float-obj.close;
     my @doubles = $obj.read-double(100);
     my $double-name = $test-output.child("double-$basename");
+    my $double-obj;
+    lives-ok { $double-obj = Audio::Sndfile.new(filename => $double-name, info => $cinfo, :w) }, "open $double-name for writing";
+    lives-ok { $i = $double-obj.write-double(@doubles) }, "write-double";
+    is($i, 100, "and it did write 100 frames");
+    lives-ok { $double-obj.close }, "close that file to reopen";
+    lives-ok { $double-obj = Audio::Sndfile.new(filename => $double-name, :r) }, "re-open $double-name for reading";
+    is($double-obj.format, $obj.format, "format is the same as we expected");
+    is($double-obj.channels, $obj.channels, "channels is what we expected");
+    is($double-obj.frames, 100, "and it has 100 frames");
+    my @read-doubles = $double-obj.read-double(100);
+    ok(@read-doubles ~~ @doubles, "and we got back the doubles we expected");
+    $double-obj.close;
     $obj.close;
-
 }
 
 done;
 
 END {
-    rm_rf $test-output;
+    rm_rf $test-output.Str;
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
